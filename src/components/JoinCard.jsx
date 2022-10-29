5bb7963af2bc293901cf9bd791bc4a67ac7f0d53
Mahmudul Hasan Selim <mhselim103@gmail.com>
2022-10-30 02:57:53 +0600

    fix:accordion

--- src/components/JoinCard.jsx
+++ src/components/JoinCard.jsx
@@ -39,7 +39,30 @@ const JoinCard = () => {
         </div>
       </div>
 
-      <div className={`h-12 ${visibility ? "block" : "hidden"}`}></div>
+      <div className={`max-h-20  ${visibility ? "block" : "hidden"}`}>
+        <div className=" grid grid-cols-2 px-14 py-2 max-w-6xl mx-auto">
+          <div className="flex gap-5 justify-around items-center">
+            <div className="bg-white w-11 h-11 rounded-full"></div>
+            <div className="flex flex-col items-center">
+              <h1 className="text-[#9B9CA3] text-base font-bold">USERNAME</h1>
+              <p className="text-lg font-poppins font-semibold">GAFI</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <h1 className="text-[#9B9CA3] text-base font-bold">AMOUNT</h1>
+              <p className="text-lg font-poppins font-semibold">PUBG</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <h1 className="text-[#9B9CA3] text-base font-bold">GAME</h1>
+              <p className="text-lg font-poppins font-semibold">10</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <h1 className="text-[#9B9CA3] text-base font-bold">PLATFOMR</h1>
+              <p className="text-lg font-poppins font-semibold">PS5</p>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
